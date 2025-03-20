1a004615001e6c28174db1a0fc1c6730cd1b4fca
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-03-20 17:00:29 +0000

    Add S6

---
+++ guioes/S6/a95485/sec1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Exercício 1
+echo "Lisboa Contents" > lisboa.txt
+echo "Porto Contents"  > porto.txt
+echo "Braga Contents"  > braga.txt
+
+# Exercício 2
+ls -l lisboa.txt
+
+# Exercício 3
+chmod 666 lisboa.txt
+
+# Exercício 4
+chmod 500 porto.txt
+
+# Exercício 5
+chmod 400 braga.txt
+
+# Exercício 6
+mkdir dir1 dir2
+
+# Exercício 7
+chmod 766 dir2
