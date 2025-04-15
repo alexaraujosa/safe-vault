a508105ae282e350b97ce0368a73f0fe44d7f572
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-04-15 23:18:36 +0100

    Update exceptions

--- trabalho-pratico/server/exceptions.py
+++ trabalho-pratico/server/exceptions.py
@@ -6,12 +6,11 @@ class PermissionDenied(Exception):
         super().__init__(self.message)
 
 
-class GroupNotFound(Exception):
-    """Raised when a group is not found in the system."""
+class UserExists(Exception):
+    """Raised when a user already exists in the system."""
 
-    def __init__(self, group_id: str):
-        """Initialize the exception with a group ID."""
-        self.message = f"Group {group_id} does not exist."
+    def __init__(self, user_id: str):
+        self.message = f"User {user_id} already exists."
         super().__init__(self.message)
 
 
@@ -23,12 +22,28 @@ class UserNotFound(Exception):
         super().__init__(self.message)
 
 
-class InvalidPermissions(Exception):
-    """Raised when invalid permissions are provided."""
+class SharedUserNotFound(Exception):
+    """Raised when a user doesn't have the shared user entry."""
 
-    def __init__(self, permission: str):
-        self.message = f"Invalid permission: {permission}.\n" \
-                       "Valid permissions are: 'r', 'w' and 'rw'."
+    def __init__(self, user_id: str, shared_user_id: str):
+        self.message = f"User {user_id} doesn't have any files shared by {shared_user_id}."
+        super().__init__(self.message)
+
+
+class GroupExists(Exception):
+    """Raised when a group already exists in the system."""
+
+    def __init__(self, group_id: str):
+        self.message = f"Group {group_id} already exists."
+        super().__init__(self.message)
+
+
+class GroupNotFound(Exception):
+    """Raised when a group is not found in the system."""
+
+    def __init__(self, group_id: str):
+        """Initialize the exception with a group ID."""
+        self.message = f"Group {group_id} does not exist."
         super().__init__(self.message)
 
 
@@ -48,14 +63,6 @@ class UserNotModeratorOfGroup(Exception):
         super().__init__(self.message)
 
 
-class GroupAlreadyExists(Exception):
-    """Raised when a group already exists in the system."""
-
-    def __init__(self, group_id: str):
-        self.message = f"Group {group_id} already exists."
-        super().__init__(self.message)
-
-
 class FileNotFoundOnVault(Exception):
     """Raised when a file is not found in a user vault."""
 
@@ -64,41 +71,9 @@ class FileNotFoundOnVault(Exception):
         super().__init__(self.message)
 
 
-class UserAlreadyExists(Exception):
-    """Raised when a user already exists in the system."""
-
-    def __init__(self, user_id: str):
-        self.message = f"User {user_id} already exists."
-        super().__init__(self.message)
-
-
-class SharedUserNotFound(Exception):
-    """Raised when a user doesn't have the shared user entry."""
-
-    def __init__(self, user_id: str, shared_user_id: str):
-        self.message = f"User {user_id} doesn't have files shared by {shared_user_id}."
-        super().__init__(self.message)
-
-
-class InvalidGroupName(Exception):
-    """Raised when a group name is invalid."""
-
-    def __init__(self, group_name: str):
-        self.message = f"Group name '{group_name}' is invalid."
-        super().__init__(self.message)
-
-
-class InvalidFileName(Exception):
-    """Raised when a file name is invalid."""
-
-    def __init__(self, file_name: str):
-        self.message = f"File name '{file_name}' is invalid."
-        super().__init__(self.message)
-
-
 class InvalidParameter(Exception):
-    """Raised when a parameter is passed to a function."""
+    """Raised when a unsupported parameter is passed to a function."""
 
-    def __init__(self, name: str, value):
-        self.message = f"Paramater '{name}' has an invalid value '{value}'."
+    def __init__(self, name: str):
+        self.message = f"Unsupported parameter: {name}."
         super().__init__(self.message)
