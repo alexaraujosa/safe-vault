a71ded6a53ea0d8396f286e98210c6fcd5aaceab
alexaraujosa <alexaraujosa@hotmail.com>
2025-04-21 12:21:58 +0100

    feat(client): added command initial processment and fixed permitted arguments length

--- trabalho-pratico/client/main.py
+++ trabalho-pratico/client/main.py
@@ -4,6 +4,9 @@ import sys
 import shlex
 import readline
 
+from common.validation import validate_params
+from client.command import process_command
+
 HISTORY_FILE = ".client_history"
 HISTORY_SIZE = 100
 
@@ -24,7 +27,7 @@ def setup_readline():
 
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print("Usage: python3 -m client.main <CERT_FILE> <PKCS12_FILE> <HOST:PORT>")
         sys.exit(1)
 
@@ -45,7 +48,18 @@ def main():
 
         args = shlex.split(command)
         print(f"Arguments: {args}")
-        # TODO Process command and send packet to server
+
+        if args[0] == "exit":
+            break
+
+        try:
+            packet = process_command(args)
+            # TODO Encrypt packet
+            # TODO Send encrypted packet to the server
+            # TODO Receive response from the server
+        except Exception as e:
+            print(e)
+            continue
 
     print("\nExiting...")
     readline.write_history_file(HISTORY_FILE)
