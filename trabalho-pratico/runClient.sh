e6b79b7a447a1dbd8ba3448c8f5a6a7e21eae56b
alexaraujosa <alexaraujosa@hotmail.com>
2025-04-27 11:55:38 +0100

    revert(scripts): removed scripts

--- trabalho-pratico/runClient.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-# DIR=${1:-"assets/projCA"}
-DIR=${1:-"test"}
-CLIENT=${2:-1}
-python3 -m ssiproject.client.client --cert $DIR/VAULT_CA.crt --keystore $DIR/VAULT_CLI$CLIENT.p12
--- trabalho-pratico/runServer.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-# DIR=${1:-"assets/projCA"}
-DIR=${1:-"test"}
-python3 -m ssiproject.server.server --cert $DIR/VAULT_CA.crt --keystore $DIR/VAULT_SERVER.p12
-[[ $? -ne 0 ]] && exit;
-echo END
