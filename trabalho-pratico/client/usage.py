1538dc164e0c0a8cfef9023a23cb85ac8437726c
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2025-05-02 17:41:29 +0100

    Update client usage

--- trabalho-pratico/client/usage.py
+++ trabalho-pratico/client/usage.py
@@ -25,8 +25,17 @@ _logs_user_global  = "logs global [-g group_id]"
 _logs_user_file    = "logs file <file-id>"
 _logs_user_group   = "logs group <group-id>"
 
-_group = f"""
-Group commands usage:
+_client = f"""Client commands usage:
+    {_add}
+    {_list}
+    {_share}
+    {_delete}
+    {_replace}
+    {_details}
+    {_revoke}
+    {_read}"""
+
+_group = f"""Group commands usage:
     {_group_create}
     {_group_delete}
     {_group_add_user}
@@ -36,29 +45,21 @@ Group commands usage:
     {_group_delete_file}
     {_group_change_permissions}
     {_group_add_moderator}
-    {_group_remove_moderator}
-"""
+    {_group_remove_moderator}"""
 
-_logs = f"""
-Log commands usage:
+_logs = f"""Log commands usage:
     {_logs_user_global}
     {_logs_user_file}
-    {_logs_user_group}
-"""
+    {_logs_user_group}"""
 
 # Full usage
-_full = f"""
-Client commands usage:
-    {_add}
-    {_list}
-    {_share}
-    {_delete}
-    {_replace}
-    {_details}
-    {_revoke}
-    {_read}
+_full = f"""{_client}
+
 {_group}
+
 {_logs}
+
 Other commands:
-    exit
-"""
+    whoami
+    help
+    exit"""
